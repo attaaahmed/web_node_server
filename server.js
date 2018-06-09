dfc49db3a82b053791d3e1ae0097ee589d340a7e
Ahmed A. Alshammary <attaaahmed@gmail.com>
2018-06-09 03:07:08 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const hbs = require('hbs');
+var app = express();
+const fs = require('fs');
+hbs.registerPartials(__dirname + '/views/partilas');
+hbs.registerHelper('getcurrentyear', () => {
+    return new Date().getFullYear();
+});
+
+
+app.use(express.static(__dirname + '/web'))
+app.set('view engine', 'hbs');
+app.engine('html', require('hbs').__express);
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}:${req.method} ${req.url}`;
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unabel to append log to the system');
+        }
+    });
+
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('main.hbs');
+// })
+app.get('/', (req, res) => {
+    res.render('site.html', {
+        pagetitle: 'Home page',
+
+    })
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pagetitle: 'About Page',
+
+    });
+})
+
+app.listen(3000, () => {
+    console.log('server is working')
+});
